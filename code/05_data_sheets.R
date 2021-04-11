da6a52345db30eb776775995704da79a89f7b18d
Sarah Gao <sgao22@usfca.edu>
2021-04-10 19:42:42 -0700

    Initial commit of data sheets script

---
+++ code/05_data_sheets.R
@@ -0,0 +1,8 @@
+# Sarah Gao
+# April 9, 2021
+# This script creates reusable data sheets based on weekly progress within
+# the experiment.
+
+# Creates a data sheet for recording constantly watered jars
+# Data sheet pre 1 week sampling
+
