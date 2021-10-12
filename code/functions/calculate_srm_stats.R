57c723900f4caccf5643093ef57b591e54f53db6
Sarah Gao <sgao22@usfca.edu>
2021-10-12 16:23:33 -0700

    Refactor into separate functions

---
+++ code/functions/calculate_srm_stats.R
@@ -0,0 +1,33 @@
+# Sarah Gao
+# October 12, 2021
+
+# This function reads in EA data and finds the
+# means and RSDs of all inputted samples.
+
+# Calculate means and RSDs for both nitrogen and carbon
+# for all SRM samples
+
+library("dplyr")
+
+calculate_srm_stats <- function(cleaned_ea_data) {
+  all_srms <- cleaned_ea_data %>%
+    filter(sample == "SRM")
+  mean_srm_n <- mean(all_srms$n_per)
+  rsd_srm_n <- sd(all_srms$n_per)*100 / mean_srm_n
+
+  mean_srm_c <- mean(all_srms$c_per)
+  rsd_srm_c <- sd(all_srms$c_per)*100 / mean_srm_c
+
+  print(paste("SRM Mean %N:", mean_srm_n))
+  print(paste("SRM N RSD:", rsd_srm_n))
+
+  print(paste("SRM Mean %C:", mean_srm_c))
+  print(paste("SRM C RSD:", rsd_srm_c))
+
+  srm_stat <- c("mean_n", "rsd_n", "mean_c", "rsd_c")
+  values <- c(mean_srm_n, rsd_srm_n, mean_srm_c, rsd_srm_c)
+  srm_stats <- data.frame(srm_stat, values)
+
+  str(srm_stats)
+  return(srm_stats)
+}
---
+++ code/functions/clean_ea_data.R
@@ -0,0 +1,32 @@
+# Sarah Gao
+# October 12, 2021
+
+# This function reads in raw EA data and cleans it up
+# to be used in downstream analyses
+
+library("dplyr")
+
+clean_ea_data <- function(input_file_name) {
+  # Read in and clean up EA data
+  # Note that reading in the csv will auto name the columns
+  # and warn you about it, hence the suppressWarnings()
+  ea_results_raw <- suppressWarnings(readr::read_csv(input_file_name))
+  ea_results_clean <- ea_results_raw %>%
+    select("X2", "X4", "X6", "X7", "X12", "X13")
+
+  ea_results_clean <- ea_results_clean[rowSums(
+    is.na(ea_results_clean)) != ncol(ea_results_clean),] %>%
+    rename("sample" = "X2", "date" = "X4", "type" = "X6", "weight" = "X7",
+           "n_per" = "X12", "c_per" = "X13") %>%
+    mutate(pos = row_number())
+
+  # Reorder the columns so position is the first column
+  ea_results_clean <- ea_results_clean[colnames(ea_results_clean)[c(7,1:6)]]
+
+  readr::write_csv(ea_results_clean, paste0("data/cleaned_data/",
+                                            tools::file_path_sans_ext(basename(
+                                              input_file_name)),
+                                            "_cleaned.csv"
+                                            ))
+  return(ea_results_clean)
+}
