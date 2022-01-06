eeba417c834673cb91178d2afed14686a9b2b30e
Sarah Gao <sgao22@usfca.edu>
2022-01-05 16:37:23 -0800

    Remove position column in clean data

--- code/functions/ea_functions/clean_ea_data.R
+++ code/functions/ea_functions/clean_ea_data.R
@@ -10,18 +10,18 @@ clean_ea_data <- function(input_file_name) {
   # Read in and clean up EA data
   # Note that reading in the csv will auto name the columns
   # and warn you about it, hence the suppressWarnings()
-  ea_results_raw <- suppressWarnings(readr::read_csv(input_file_name, col_names = FALSE))
+  ea_results_raw <- suppressWarnings(
+    readr::read_csv(input_file_name, col_names = FALSE))
   ea_results_clean <- ea_results_raw %>%
     select("X2", "X4", "X6", "X7", "X12", "X13")
 
   ea_results_clean <- ea_results_clean[rowSums(
     is.na(ea_results_clean)) != ncol(ea_results_clean), ] %>%
-    rename("sample" = "X2", "date" = "X4", "type" = "X6", "weight" = "X7",
-           "n_per" = "X12", "c_per" = "X13") %>%
-    mutate(pos = row_number())
+    rename("sample_no" = "X2", "date" = "X4", "type" = "X6", "weight" = "X7",
+           "n_per" = "X12", "c_per" = "X13")
 
   # Reorder the columns so position is the first column
-  ea_results_clean <- ea_results_clean[colnames(ea_results_clean)[c(7, 1:6)]]
+  # ea_results_clean <- ea_results_clean[colnames(ea_results_clean)[c(7, 1:6)]]
 
   readr::write_csv(ea_results_clean, paste0("data/cleaned_data/",
                                             tools::file_path_sans_ext(basename(
