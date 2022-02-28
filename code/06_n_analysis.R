b73bc008ec00dee8b3615512d7f4d0615dc95b87
Sarah Gao <sgao22@usfca.edu>
2022-02-28 15:47:02 -0800

    Simplify n data summarization

--- code/06_n_analysis.R
+++ code/06_n_analysis.R
@@ -6,6 +6,7 @@
 # hellosarahgao@gmail.com
 
 library("ggplot2")
+library("tidyverse")
 
 # Source and run function to clean N data
 source("code/functions/n_functions/n_clean_n_data.R")
@@ -13,24 +14,28 @@ n_data_clean <- clean_n_data(
   "data/raw_data/SmartChem_N_extractions/20210212_Gao_1_export.csv")
 
 # Source and run function to analyze stats for samples
-source("code/functions/n_functions/n_calculate_sample_stats.R")
-n_sample_stats <- n_calculate_sample_stats(n_data_clean)
+# source("code/functions/n_functions/n_calculate_sample_stats.R")
+# n_sample_stats <- n_calculate_sample_stats(n_data_clean)
 
 # Source and run function that compiles all the bad jar assignment
 # csvs from Spring 2021
 source("code/functions/compile_sample_assignments.R")
 all_treatments <- compile_sample_assignments()
 
-# Source  and run function that organizes and aggregates n results to
-# master list of treatments
-source("code/functions/map_results.R")
-all_treatments_n <- add_results(all_treatments, n_sample_stats)
-
-# Calculate means of each drying x cc treatment group across replicates
-n_results_means <- all_treatments_n %>%
-  group_by(drying_treatment, cc_treatment, pre_post_wet) %>%
-  summarise(mean_nh3 = mean(mean_nh3), mean_no2 = mean(mean_no2),
-            mean_no2_no3 = mean(mean_no2_no3))
+# Summarize data (i.e. calculate means and SDs) and map to all_treatments
+n_data_stats <- n_data_clean %>%
+  mutate(no3 = no2_no3 - no2) %>%
+  group_by(sample_no) %>%
+  mutate(no3 = no2_no3 - no2) %>%
+  summarize(mean_nh3 = mean(nh3),
+            sd_nh3 = sd(nh3),
+            mean_no2 = mean(no2),
+            sd_no2 = sd(no2),
+            mean_no3 = mean(no3),
+            sd_no3 = sd(no3),
+            mean_no2_no3 = mean(no2_no3),
+            sd_no2_no3 = sd(no2_no3)) %>%
+  left_join(all_treatments)
 
 # Set plot themes
 theme_update(plot.title = element_text(face = "bold",
@@ -63,23 +68,32 @@ theme_update(plot.title = element_text(face = "bold",
              legend.key.size = unit(8, "pt")
 )
 
-# Create a plot comparing NO2-NO3 levels between groups with and
+# Create a plot comparing NH3 levels between groups with and
 # without cover crops at 4 weeks
-no2_no3_compare_cc_plot <- n_results_means %>%
-  filter(drying_treatment == "four_wk") %>%
-  ggplot(aes(x = pre_post_wet, y = mean_no2_no3, fill = cc_treatment)) +
+no2_no3_compare_cc_plot <- n_data_stats %>%
+  filter(pre_post_wet != "no soil") %>%
+  group_by(pre_post_wet, cc_treatment) %>%
+  summarize(mean_mean_nh3 = mean(mean_nh3),
+            sd_mean_nh3 = sd(mean_nh3)) %>%
+  ggplot(aes(x = pre_post_wet,
+             y = mean_mean_nh3,
+             fill = cc_treatment)) +
   geom_col(position = position_dodge()) +
-  scale_y_continuous() +
+  geom_errorbar(aes(ymax = mean_mean_nh3 + sd_mean_nh3,
+                    ymin = mean_mean_nh3 - sd_mean_nh3),
+                position = position_dodge()) +
   scale_x_discrete(limits = c("cw", "pre", "post"),
                    labels = c(
                      "Constant Water", "Pre Wetting", "Post Wetting")) +
-  labs(title = "NO2-NO3 in Samples With and Without Cover Crop Residue",
+  labs(title = paste("NH3 in Samples With and Without Cover Crop\n",
+                     " Residue After 4 Weeks of Drying"),
        x = "Water Treatments", y = "NO2-NO3 (ppm)",
        fill = "Cover Crop Treatment") +
-  scale_fill_discrete(labels = c("Without cover crop", "With cover crop"))
+  scale_fill_discrete(breaks = c("w_cc", "no_cc"),
+                      labels = c("Without cover crop", "With cover crop"))
 
-ggsave(no2_no3_compare_cc_plot,
-         filename = "output/2021/n_plots/no2_no3_v_cc.png")
+ggsave(nh3_compare_cc_plot,
+         filename = "output/2021/n_plots/nh3_v_cc.png")
 
 # Create a plot comparing NH3 levels between groups with and
 # without cover crops at 4 weeks
@@ -91,7 +105,8 @@ nh3_compare_cc_plot <- n_results_means %>%
   scale_x_discrete(limits = c("cw", "pre", "post"),
                    labels = c(
                      "Constant Water", "Pre Wetting", "Post Wetting")) +
-  labs(title = "NH3 in Samples With and Without Cover Crop Residue",
+  labs(title = paste("NH3 in Samples With and Without Cover Crop Residue",
+                     "After 4 Weeks of Drying"),
        x = "Water Treatments", y = "NH3 (ppm)",
        fill = "Cover Crop Treatment") +
   scale_fill_discrete(labels = c("Without cover crop", "With cover crop"))
@@ -109,7 +124,8 @@ no2_compare_cc_plot <- n_results_means %>%
   scale_x_discrete(limits = c("cw", "pre", "post"),
                    labels = c(
                      "Constant Water", "Pre Wetting", "Post Wetting")) +
-  labs(title = "NO2 in Samples With and Without Cover Crop Residue",
+  labs(title = paste("NO2 in Samples With and Without Cover Crop Residue",
+                     "After 4 Weeks of Drying"),
        x = "Water Treatments", y = "NO2 (ppm)",
        fill = "Cover Crop Treatment") +
   scale_fill_discrete(labels = c("Without cover crop", "With cover crop"))
