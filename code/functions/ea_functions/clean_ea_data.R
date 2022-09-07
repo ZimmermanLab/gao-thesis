ed9af9f5e02ceaf340e371a1af7653b40beb6d41
Sarah Gao <sgao22@usfca.edu>
2022-09-07 12:36:56 -0700

    Update clean EA function for new data

--- code/functions/ea_functions/clean_ea_data.R
+++ code/functions/ea_functions/clean_ea_data.R
@@ -5,31 +5,26 @@
 # hellosarahgao@gmail.com
 
 library("dplyr")
+library("readxl")
 
-input_file_name <- "data/raw_data/EA_CN/20210727/210727_Run.csv"
-
-clean_ea_data <- function(input_file_name) {
+clean_ea_data <- function(input_file_list) {
   # Read in and clean up EA data
   # Note that reading in the csv will auto name the columns
   # and warn you about it, hence the suppressWarnings()
-  ea_results_raw <- suppressWarnings(
-    readr::read_csv(input_file_name, col_names = FALSE))
+  ea_results_raw <- input_file_list %>%
+    lapply(read_xls, col_names = FALSE) %>%
+    bind_rows()
 
   # Select only sample number, type of sample, and n and c percentages
   ea_results_clean <- ea_results_raw %>%
-    select("X2", "X6", "X12", "X13") %>%
-    rename("sample_no" = "X2", "type" = "X6",
-           "n_per" = "X12", "c_per" = "X13") %>%
+    select("...2", "...6", "...12", "...13") %>%
+    rename("sample_no" = "...2", "type" = "...6",
+           "n_mg" = "...12", "c_mg" = "...13") %>%
     drop_na() %>%
     mutate(pos = row_number())
 
   # Reorder the columns so position is the first column
   ea_results_clean <- ea_results_clean[colnames(ea_results_clean)[c(5, 1:4)]]
 
-  readr::write_csv(ea_results_clean, paste0("data/cleaned_data/",
-                                            tools::file_path_sans_ext(basename(
-                                              input_file_name)),
-                                            "_cleaned.csv"
-                                            ))
   return(ea_results_clean)
 }
