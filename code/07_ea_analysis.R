7392aedb6e9e132f0c3d425e8e7fa1b6c37420b3
Sarah Gao <sgao22@usfca.edu>
2022-09-07 13:27:03 -0700

    Add plots and prelim ANOVA analysis

--- code/07_ea_analysis.R
+++ code/07_ea_analysis.R
@@ -19,13 +19,21 @@ library("readxl")
 # First, we will examine the SRM data to see if there was any drift / trend
 # over the run and to determine if we need any correcting factors.
 
+# Compile list of file paths of EA data
+files <- list.files(path = "data/raw_data/EA_CN/2022",
+                    recursive = TRUE, full.names = TRUE,
+                    pattern = "\\w+_Run\\d_\\d{2}.(xls|XLS)")
+
 # Read in and clean up EA data
 source("code/functions/ea_functions/clean_ea_data.R")
-ea_results_clean <- clean_ea_data(
-  "data/raw_data/EA_CN/20210727/210727_Run.csv")
+ea_results_clean <- clean_ea_data(files) %>%
+  filter(!(sample_no == "SRM" & n_mg > 0.8)) %>%
+# High SRM N values from 6/7/22 probably means should be rerun
+  filter(date != "06/07/2022")
 
 # Calculate means and RSDs for both nitrogen and carbon
 # for all SRM samples
+# From Calla: RSDs for both N and C should be 5-10%
 source("code/functions/ea_functions/ea_calculate_srm_stats.R")
 srm_stats <- calculate_srm_stats(ea_results_clean)
 
@@ -59,84 +67,52 @@ ggsave(filename = "output/ea_plots/srm_c_plot.png", srm_c_plot)
 source("code/functions/ea_functions/ea_calculate_sample_stats.R")
 sample_stats <- calculate_sample_stats(ea_results_clean)
 
-# Calculate the mean and median RSD across all samples
-sample_rsd <- sample_stats %>%
-  filter(is.na(rsd_c) == FALSE) %>%
-  select(rsd_c, rsd_n)
+# Pull out list of questionable samples that need to be rerun
+# based on high RSDs
+need_rerun <- sample_stats %>%
+  left_join(ea_results_clean) %>%
+  filter(flag == "yes")
 
-# Calculate the mean and median rsds for both C and N across all samples
-# Not sure why I put this in here or why it's necessary but nice to have maybe?
-median_sample_rsd_c <- median(sample_rsd$rsd_c)
-mean_sample_rsd_c <- mean(sample_rsd$rsd_c)
-
-median_sample_rsd_n <- median(sample_rsd$rsd_n)
-mean_sample_rsd_n <- mean(sample_rsd$rsd_n)
-
-# Remove RSDs and clean up sample names
-samples_only <- sample_stats %>%
-  select(sample_no, mean_n, mean_c)
-samples_only$sample_no <- str_sub(samples_only$sample_no, -3)
+# Clean up sample names
+sample_stats$sample_no <- as.numeric(str_sub(
+  sample_stats$sample_no, start = -3))
 
 # Map samples and results to master list of treatments
-source("code/functions/compile_sample_assignments.R")
-all_treatments <- compile_sample_assignments()
+all_treatments <- read_csv("output/2022/jar_assignments/master_list.csv") %>%
+  transform(sample_no = as.numeric(sample_no))
 
-source("code/functions/map_results.R")
-all_treatments_c_n <- add_results(all_treatments, samples_only)
+mapped_results <- sample_stats %>%
+  left_join(all_treatments)
 
-# Calculate means of each drying x cc treatment group across replicates
-c_n_results_means <- all_treatments_c_n %>%
+# Calculate means of each drying x cc treatment group across replicates and
+# calculate C:N ratios
+c_n_results_means <- mapped_results %>%
   group_by(drying_treatment, cc_treatment, pre_post_wet) %>%
-  summarise(mean_n = mean(mean_n), mean_c = mean(mean_c))
+  summarise(mean_n = mean(mean_n), mean_c = mean(mean_c)) %>%
+  mutate("c_n_ratio" = mean_c / mean_n)
 
-# Create a plot comparing N levels between groups with and
-# without cover crops at 4 weeks
-n_compare_cc_plot <- c_n_results_means %>%
-  filter(drying_treatment == "four_wk") %>%
-  ggplot(aes(x = pre_post_wet, y = mean_n, fill = cc_treatment)) +
+# Create a plot comparing C:N ratios
+c_n_plot <- c_n_results_means %>%
+  filter(pre_post_wet != "no_soil") %>%
+  filter(pre_post_wet != "all_dry") %>%
+  group_by(pre_post_wet, cc_treatment, drying_treatment) %>%
+  ggplot(aes(x = pre_post_wet,
+             y = c_n_ratio,
+             fill = cc_treatment)) +
   geom_col(position = position_dodge()) +
-  scale_y_continuous() +
-  scale_x_discrete(limits = c("cw", "post"),
-                   labels = c(
-                     "Constant Water", "Post Wetting")) +
-  labs(title = paste("Total Nitrogen in Samples With and Without Cover Crop",
-                     "Residue After 4 Weeks of Drying"),
-       x = "Water Treatments", y = "Total Nitrogen (%)",
-       fill = "Cover Crop Treatment") +
-  scale_fill_discrete(labels = c("Without cover crop", "With cover crop"))
+  facet_grid(. ~ factor(drying_treatment,
+                        levels = c("initial", "one_wk", "two_wk",
+                                   "four_wk", "all_dry"))) +
+  coord_cartesian(ylim=c(5, 9))
 
-ggsave(n_compare_cc_plot,
-       filename = "output/2021/ea_plots/n_v_cc.png")
 
-# Create a plot comparing C levels between groups with and
-# without cover crops at 4 weeks
-c_compare_cc_plot <- c_n_results_means %>%
-  filter(drying_treatment == "four_wk") %>%
-  ggplot(aes(x = pre_post_wet, y = mean_c, fill = cc_treatment)) +
-  geom_col(position = position_dodge()) +
-  scale_y_continuous() +
-  scale_x_discrete(limits = c("cw", "post"),
-                   labels = c(
-                     "Constant Water", "Post Wetting")) +
-  labs(title = paste("Total Carbon in Samples With and Without Cover Crop",
-                     "Residue After 4 Weeks of Drying"),
-       x = "Water Treatments", y = "Total Carbon (%)",
-       fill = "Cover Crop Treatment") +
-  scale_fill_discrete(labels = c("Without cover crop", "With cover crop"))
+#  scale_x_discrete(limits = c("all_dry", "cw", "pre", "post"),
+#                   labels = c("All Dry", "Constant Water", "Pre Wetting",
+#                              "Post Wetting"))
 
-ggsave(c_compare_cc_plot,
-       filename = "output/2021/ea_plots/c_v_cc_.png")
-
-#########
-
-# Compare C:N ratios
-samples_only %>%
-  left_join(all_treatments) %>%
-  filter(drying_treatment == "four_wk") %>%
-  filter(pre_post_wet == "post") %>%
-  group_by(cc_treatment) %>%
-  summarise(mean_mean_c = mean(mean_c),
-            mean_mean_n = mean(mean_n)) %>%
-  ggplot(aes(x = cc_treatment,
-             y = mean_mean_c / mean_mean_n)) +
-  geom_col(position = position_dodge())
+# Run statistical analysis on C:N, excluding cw and no_soil jars
+ea_final_stats <- c_n_results_means %>%
+  filter(pre_post_wet != "no_soil") %>%
+  filter(pre_post_wet != "cw") %>%
+  lm(data = ., c_n_ratio ~ pre_post_wet * cc_treatment) %>%
+  anova()
