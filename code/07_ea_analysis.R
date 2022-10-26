6e81d7ec08e3dc14a06f237dbf0fd63e8906a013
Sarah Gao <sgao22@usfca.edu>
2022-10-25 21:41:01 -0700

    Subset EA data for clearer story

--- code/07_ea_analysis.R
+++ code/07_ea_analysis.R
@@ -15,11 +15,6 @@ library("fs")
 
 ############
 
-# SRMs
-
-# First, we will examine the SRM data to see if there was any drift / trend
-# over the run and to determine if we need any correcting factors.
-
 # Compile list of file paths of percentage EA data
 files_percent <- dir_ls(path = "data/raw_data/EA_CN/2022/",
                 recurse = 1,
@@ -29,6 +24,10 @@ files_percent <- dir_ls(path = "data/raw_data/EA_CN/2022/",
 # Filter out any weird sample runs that had 0
 source("code/functions/ea_functions/clean_ea_data.R")
 cn_percent_clean <- clean_ea_data(files_percent)
+
+# SRMs
+# Examine the SRM data to see if there was any drift / trend
+# over time and to determine if we need any correcting factors.
 
 # Calculate means and RSDs for both nitrogen and carbon
 # for all SRM samples
@@ -81,8 +80,8 @@ compiled_results <- mapped_results %>%
             sd_mean_cn = sd(mean_c_n),
             rsd_mean_cn = sd_mean_cn / mean_mean_cn * 100)
 
-# Create a plot comparing C:N ratios
-c_n_plot <- compiled_results %>%
+# Create a plot comparing C:N ratios with all factors
+c_n_plot_all <- compiled_results %>%
   filter(pre_post_wet != "no_soil") %>%
   group_by(pre_post_wet, cc_treatment, drying_treatment) %>%
   ggplot(aes(x = pre_post_wet)) +
@@ -93,26 +92,91 @@ c_n_plot <- compiled_results %>%
                     ymin = mean_mean_cn - sd_mean_cn,
                     group = cc_treatment),
                 position = position_dodge()) +
-  coord_cartesian(ylim = c(0, 23)) +
+  coord_cartesian(ylim = c(16, 23)) +
   facet_grid(. ~ factor(drying_treatment,
                         levels = c("initial", "one_wk", "two_wk",
                                    "four_wk", "all_dry"))) +
-  scale_x_discrete(limits = c("all_dry","initial", "cw", "pre", "post"))
+  scale_x_discrete(limits = c("all_dry","initial", "cw", "pre", "post"),
+                   labels = c("All-Dry", "Initial", "Constant",
+                              "Pre-Wet", "Post-Wet"))
 
-
-#  scale_x_discrete(limits = c("all_dry", "cw", "pre", "post"),
-#                   labels = c("All Dry", "Constant Water", "Pre Wetting",
-#                              "Post Wetting"))
-
-# Run statistical analysis on C:N, excluding cw and no_soil jars
-ea_final_stats <- mapped_results %>%
+# Subset data to look at changes over time in constantly watered samples
+constant_time <- compiled_results %>%
   filter(pre_post_wet != "no_soil") %>%
-  filter(pre_post_wet != "cw") %>%
-  filter(pre_post_wet != "all_dry") %>%
-  glm(data = ., mean_c_n ~ drying_treatment) %>%
-  summary()
+  filter(pre_post_wet == "initial" |
+           pre_post_wet == "pre") %>%
+  filter(drying_treatment == "initial" |
+           drying_treatment == "one_wk" |
+           drying_treatment == "two_wk" |
+           drying_treatment == "four_wk")
+constant_time_plot <- constant_time  %>%
+  group_by(pre_post_wet, cc_treatment, drying_treatment) %>%
+  ggplot(aes(x = pre_post_wet)) +
+  geom_col(aes(y = mean_mean_cn,
+               fill = cc_treatment),
+           position = position_dodge()) +
+  geom_errorbar(aes(ymax = mean_mean_cn + sd_mean_cn,
+                    ymin = mean_mean_cn - sd_mean_cn,
+                    group = cc_treatment),
+                position = position_dodge()) +
+  labs(title = "Constantly Watered Samples Over Time") +
+  coord_cartesian(ylim = c(16, 23)) +
+  facet_grid(. ~ factor(drying_treatment,
+                        levels = c("initial", "one_wk", "two_wk",
+                                   "four_wk", "all_dry")))
 
-# Subset data to look at one thing across time
-# or within a timeframe look at pre/post wet
-# or a pre/post wet comparing cc treatment
-# Have targeted q's: eg is there interaction b/w week and pre/post
+# Subset data to look at effect of rewetting by comparing changes
+# at one, two, and four weeks between pre and post wet samples
+pre_post_diff <- compiled_results %>%
+  filter(pre_post_wet != "no_soil") %>%
+  filter(pre_post_wet == "pre" |
+           pre_post_wet == "post") %>%
+  filter(drying_treatment == "one_wk" |
+           drying_treatment == "two_wk" |
+           drying_treatment == "four_wk")
+pre_post_diff_plot <- pre_post_diff %>%
+  group_by(pre_post_wet, cc_treatment, drying_treatment) %>%
+  ggplot(aes(x = pre_post_wet)) +
+  geom_col(aes(y = mean_mean_cn,
+               fill = cc_treatment),
+           position = position_dodge()) +
+  geom_errorbar(aes(ymax = mean_mean_cn + sd_mean_cn,
+                    ymin = mean_mean_cn - sd_mean_cn,
+                    group = cc_treatment),
+                position = position_dodge()) +
+  labs(title = "Effect of Rewetting Across Time") +
+  coord_cartesian(ylim = c(16, 23)) +
+  facet_grid(. ~ factor(drying_treatment,
+                        levels = c("one_wk", "two_wk",
+                                   "four_wk", "all_dry"))) +
+  scale_x_discrete(limits = c("pre", "post"),
+                   labels = c("Pre-Wet", "Post-Wet"))
+
+# Subset data to look at effect of drying over time by looking at
+# only dried samples
+drying_diff <- compiled_results %>%
+  filter(pre_post_wet != "no_soil") %>%
+  filter(pre_post_wet == "initial" |
+           pre_post_wet == "pre" |
+           pre_post_wet == "all_dry") %>%
+  filter(drying_treatment == "one_wk" |
+           drying_treatment == "two_wk" |
+           drying_treatment == "four_wk" |
+           drying_treatment == "all_dry")
+drying_diff_plot %>% drying_diff %>%
+  group_by(pre_post_wet, cc_treatment, drying_treatment) %>%
+  ggplot(aes(x = pre_post_wet)) +
+  geom_col(aes(y = mean_mean_cn,
+               fill = cc_treatment),
+           position = position_dodge()) +
+  geom_errorbar(aes(ymax = mean_mean_cn + sd_mean_cn,
+                    ymin = mean_mean_cn - sd_mean_cn,
+                    group = cc_treatment),
+                position = position_dodge()) +
+  labs(title = "Effect of Drying Over Time") +
+  coord_cartesian(ylim = c(16, 23)) +
+  facet_grid(. ~ factor(drying_treatment,
+                        levels = c("one_wk", "two_wk",
+                                   "four_wk", "all_dry"))) +
+  scale_x_discrete(limits = c("pre", "post", "all_dry"),
+                   labels = c("Pre-Wet", "Post-Wet", "All-Dry"))
