aee900562f1237b5672a7946382e852141d17fb0
Sarah Gao <sgao22@usfca.edu>
2022-10-27 12:38:55 -0700

    Add %N and %C to analyses

--- code/functions/ea_functions/ea_calculate_sample_stats.R
+++ code/functions/ea_functions/ea_calculate_sample_stats.R
@@ -15,20 +15,21 @@ calculate_sample_stats <- function(cleaned_ea_data) {
   # Collect all the samples into one dataframe and create a column that checks
   # for matched names
   all_samples <- cleaned_ea_data %>%
-    select(sample_no, c_n_ratio) %>%
+    select(sample_no, c_n_ratio, n_per, c_per) %>%
     filter(!(sample_no == "SRM" |
                sample_no == "BLANK" |
                sample_no == "Blank" |
                grepl("Blank", sample_no) == TRUE |
                sample_no == "Bypass")) %>%
     filter(!str_detect(sample_no, "^ASP")) %>%
-    replace(is.na(.), 0) %>%
     arrange(sample_no)
 
   ea_stats <- all_samples %>%
     group_by(sample_no) %>%
-    summarize(mean_c_n = mean(c_n_ratio),
-              sd_c_n = sd(c_n_ratio))
+    summarize(mean_cn = mean(c_n_ratio),
+              sd_cn = sd(c_n_ratio),
+              mean_nper = mean(n_per),
+              mean_cper = mean(c_per))
   # Clean up sample names
   ea_stats$sample_no <- as.numeric(str_sub(
     ea_stats$sample_no, start = -3))
--- code/functions/ea_functions/summarize_treatments.R
+++ code/functions/ea_functions/summarize_treatments.R
@@ -10,7 +10,11 @@ library("dplyr")
 summarize_treatments <- function(sample_stats) {
   compiled <- sample_stats %>%
     group_by(drying_treatment, cc_treatment, pre_post_wet) %>%
-    summarise(mean_mean_cn = mean(mean_c_n),
-              sd_mean_cn = sd(mean_c_n))
+    summarise(mean_mean_cn = mean(mean_cn),
+              sd_mean_cn = sd(mean_cn),
+              mean_mean_nper = mean(mean_nper),
+              sd_mean_nper = sd(mean_nper),
+              mean_mean_cper = mean(mean_cper),
+              sd_mean_cper = sd(mean_cper))
   return(compiled)
 }
