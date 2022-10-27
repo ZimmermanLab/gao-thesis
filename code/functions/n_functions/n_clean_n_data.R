3bfb63cfe22fb866eae10ff7aef91909dc167faa
Sarah Gao <sgao22@usfca.edu>
2022-10-26 17:37:14 -0700

    Clean up pivot_wider script

--- code/functions/n_functions/n_clean_n_data.R
+++ code/functions/n_functions/n_clean_n_data.R
@@ -11,44 +11,45 @@ library("tidyr")
 library("stringi")
 library("stringr")
 
-clean_n_data <- function(input_file_name) {
+clean_n_data <- function(input_files) {
 
 # Read in SmartChem data.
 # Note that all values are concentrations in ppm or mg/L.
-n_data_clean <- read_delim(input_file_name, col_names = FALSE, delim = ";") %>%
+n_data_clean <- read_delim(input_files, delim = ";", col_names = FALSE) %>%
   select(X1, X4, X7, X8) %>%
   rename("sample_no_full" = X1,
-         "concentration_mg/L" = X4,
+         "concentration_mg_l" = X4,
          "run_time" = X7,
          "type" = X8) %>%
-  pivot_wider(names_from = "type", values_from = "concentration_mg/L") %>%
+  # Use only the date, not time, for run time
   mutate(run_time = str_sub(run_time, end = 10)) %>%
+  # Filter out weird bad runs
+  filter(concentration_mg_l != -9999) %>%
+  # Filter out standards
+  filter(sample_no_full != "NO2E6",
+         sample_no_full != "NO3E6",
+         str_detect(sample_no_full, "Blank") == FALSE) %>%
   group_by(sample_no_full) %>%
-  # summarise_all(list(~first(na.omit(.)))) %>%
+  pivot_wider(names_from = "type", values_from = "concentration_mg_l") %>%
+  ungroup(sample_no_full) %>%
   rename("nh3" = "WNHR",
          "no2_no3" = "WNO6") %>%
+  # Add column to show sample type
   mutate("sample_type" = case_when(
     grepl("^[0-9]{3}E", sample_no_full) == TRUE ~ "extract",
-    grepl("^[0-9]{3}L", sample_no_full) == TRUE ~ "leachate",
-    grepl("^Blank.*L", sample_no_full) == TRUE ~ "blank_leachate",
-    grepl("^Blank.*E", sample_no_full) == TRUE ~ "blank_extract",
-    grepl("^NO2", sample_no_full) == TRUE ~ "no2_standard",
-    grepl("^NO3", sample_no_full) == TRUE ~ "no3_standard")) %>%
-  ungroup() %>%
-  arrange(sample_no_full)
-
-# Replace all negative values with 0
-n_data_clean <- replace(n_data_clean, n_data_clean < 0, 0)
-
-# Separate sample ID number and replicate number
-n_data_clean <- n_data_clean %>%
+    grepl("^[0-9]{3}L", sample_no_full) == TRUE ~ "leachate")) %>%
+  # Separate out replicate number
   mutate("sample_no" = case_when(
     grepl("^[0-9]", sample_no_full) == TRUE ~
       as.numeric(str_sub(sample_no_full, end = 3)))) %>%
   mutate("rep_no" = case_when(
     grepl("^[0-9]", sample_no_full) == TRUE ~
       as.numeric(str_sub(sample_no_full, start = -1)))) %>%
-  select(-c(sample_no_full))
+  select(-c(sample_no_full)) %>%
+  relocate(c(sample_no, rep_no))
+
+# Replace all negative values with 0
+n_data_clean[n_data_clean < 0] <- 0
 
 return(n_data_clean)
 }
