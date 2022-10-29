21ccd6c8e11a810d446b6f80e618b80e9d25bb5d
Sarah Gao <sgao22@usfca.edu>
2022-10-29 11:53:27 -0700

    Create find peaks function from peak.gas library

--- code/08_co2_analysis.R
+++ code/08_co2_analysis.R
@@ -6,28 +6,64 @@
 # hellosarahgao@gmail.com
 
 # Load packages
-library(readr)
-library(tidyr)
-library(dplyr)
-library(ggplot2)
-library(MESS)
+library("readr")
+library("tidyr")
+library("dplyr")
+library("ggplot2")
+library("stringr")
 
-# Read in the data
-hr1_01_raw <- read.csv(
-  "data/raw_data/LICOR_CO2/20220111_tests/20220111_01_1hr_notime.txt",
-  sep = "") %>%
-  select(System_Time_.h.m.s., CO._.µmol_mol...,
-         CO._Absorption, Flow_Rate_.L_min...) %>%
-  rename(time = System_Time_.h.m.s., co2_ppm = CO._.µmol_mol...,
-         co2_absorption = CO._Absorption, flow_rate = Flow_Rate_.L_min...)
+# Source function to calculate AUCs
+source("code/functions/co2_functions/find_peaks.R")
 
-# Replace negative CO2 values with 0
-hr1_01_raw$co2_ppm[hr1_01_raw$co2_ppm < 0] <- 0
+# Create list of file paths of CO2 data files
+files_list <- list.files(path = "data/cleaned_data/LICOR/", recursive = FALSE,
+                         pattern = "*.txt")
+files_list <- subset(files_list, str_detect(files_list, "extract") == FALSE)
 
-spline.d <- as.data.frame(spline(hr1_01_raw$time, hr1_01_raw$co2_ppm))
-ggplot(hr1_01_raw, aes(x = time,
-                       y = co2_ppm)) +
-  geom_point() +
-  geom_smooth(color = "blue", method="loess", se=TRUE, fullrange=FALSE, level=0.95)
-require(MESS)
-auc(hr1_01_raw$time, hr1_01_raw$co2_ppm, type = 'spline', from = "16:2:22", to = "16:2:26")
+auc_summary_all <- data.frame()
+
+for(f in 1:length(files_list)) {
+  auc_summary <- find_peaks(files_list[f])
+  auc_summary_all <- rbind(auc_summary_all, auc_summary)
+}
+
+# Create separate list of standards
+auc_stds <- auc_summary_all %>%
+  filter(str_detect(sample, "[0-9]{3}_[0-9]{2}") == FALSE)
+
+# Separate out replicate number from sample number
+auc_samples <- auc_summary_all %>%
+  # Select samples (non-stds) only
+  filter(str_detect(sample, "[0-9]{3}_[0-9]{2}") == TRUE) %>%
+  mutate(sample_no = str_sub(sample, end = 3)) %>%
+  mutate(rep_no = str_sub(sample, start = -2)) %>%
+  select(-sample) %>%
+  relocate(sample_no, total_auc)
+
+# Determine outliers in sample reps
+samples_outliers <- auc_samples %>%
+  group_by(sample_no, date) %>%
+  mutate(median = median(total_auc),
+            iqr = IQR(total_auc)) %>%
+  mutate(outlier_flag = case_when(total_auc > median + (1.5 * iqr) |
+                                    total_auc < median - (1.5 * iqr)
+                                  ~ "moderate",
+                                  total_auc > median + (3 * iqr) |
+                                    total_auc < median - (3 * iqr)
+                                  ~ "extreme")) %>%
+  arrange(sample_no, date)
+
+samples_outliers$sample_no <- as.double(samples_outliers$sample_no)
+
+# Bring in all treatment assignments
+all_treatments <- read_csv("output/2022/jar_assignments/master_list.csv")
+# Map to samples
+samples_mapped <- samples_outliers %>%
+  left_join(all_treatments)
+
+treatments_all <- samples_mapped
+# Filter out outliers
+treatments_no_mod <- samples_mapped %>%
+  filter(is.na(outlier_flag))
+
+
---
+++ code/functions/co2_functions/find_peaks.R
@@ -0,0 +1,109 @@
+# This function takes in a .txt file with LICOR CO2 data and finds the area
+# under the curve.
+
+# This is modified from Jacob Anderson's peak.gas package:
+# https://github.com/andersonjake1988/peak.gas
+
+
+find_peaks <- function(file_path, cut.off = 2) {
+
+  # Creates a Peaks function that identifies CO2 readings that are higher
+  # than the manually set cut.off threshold (baseline)
+  Peaks <- function(x){
+    output <- vector()
+    for(i in 1:length(x)){
+      ifelse(x[i] >= cut.off, output[i] <- x[i], output[i] <- NA)
+    }
+    output
+  }
+
+  # Reads in file
+  raw_file <- read.csv(paste0("data/cleaned_data/LICOR/", file_path),
+                       header = TRUE, sep = "\t", fill = TRUE,
+                strip.white = TRUE, check.names = FALSE)
+
+  data.1 <- raw_file %>%
+    # Selects only Date, Time, and CO2
+    select(1:3) %>%
+    rename("Date" = 1,
+           "Time" = 2,
+           "CO2" = 3) %>%
+    # Make sure to filter out any extra headers embedded in the data
+    filter(Date != "System_Date_(Y-M-D)") %>%
+    filter(!(str_detect(Date, "at"))) %>%
+    mutate(Time = paste(Date, Time)) %>%
+    select(-Date)
+  # Convert times into doubles
+  data.1$Time <- lubridate::as_datetime(data.1$Time)
+  # Ensure all columns are double
+  data.1$CO2 <- as.double(data.1$CO2)
+
+  # Identify rows that will be used for peak calculation
+  test.2 <- data.1 %>%
+    mutate("Peaks" = Peaks(CO2)) %>%
+    mutate("Value" = !is.na(Peaks), "Replicate" = NA)
+
+  # Add time column
+  test.2 <- arrange(test.2, Time)
+  r <- 0
+  for(i in 1:(length(test.2$Value)-1)){
+    if(test.2$Value[i] == TRUE & test.2$Value[i+1] == TRUE){
+      test.2$Replicate[i] <- r
+    } else if(test.2$Value[i] == FALSE & test.2$Value[i+1] == TRUE){
+      r <- r + 1
+    } else if(test.2$Value[i] == FALSE & test.2$Value[i+1] == FALSE){
+      test.2$Replicate[i] <- NA
+    } else {
+      test.2$Replicate[i] <- r
+    }
+  }
+
+  # Sets baseline to cut.off value (default = 2)
+  for(i in 2:(length(test.2$Value)-1)){
+    if(test.2$Value[i] == FALSE & test.2$Value[i+1] == TRUE){
+      test.2$Peaks[i] <- cut.off
+      test.2$Replicate[i] <- test.2$Replicate[i+1]
+    } else if(test.2$Value[i] == FALSE & test.2$Value[i-1] == TRUE){
+      test.2$Peaks[i] <- cut.off
+      test.2$Replicate[i] <- test.2$Replicate[i-1]
+    } else {
+      next
+    }
+  }
+  test.3 <- na.omit(test.2)
+  test.3 <- mutate(test.3, "Area" = 0)
+  test.3$diff <- c(0, diff(test.3$Peaks))
+  peaks <- test.3$Peaks-cut.off
+  diff <- c(0, diff(peaks))
+  for(i in 1:(length(test.3$Peaks)-1)){
+    if(test.3$Replicate[i] == test.3$Replicate[i+1]){
+      time <- as.numeric(difftime(test.3$Time[i+1], test.3$Time[i]))
+      # Finds area at the beginning of the curve
+      if(test.3$Value[i] == FALSE & test.3$Value[i+1] == TRUE){
+        test.3$Area[i] <- (time * diff[i+1])/2
+        # Finds are between curves
+      } else if(test.3$Value[i] == FALSE & test.3$Value[i+1] == FALSE){
+        test.3$Area[i] <- (time * diff[i])/2
+      } else if(test.3$Value[i] == TRUE){
+        test.3$Area[i] <- (peaks[i] * time) + ((time * diff[i+1])/2)
+      }
+    } else{
+      next
+    }
+  }
+  test.4 <- test.3 %>%
+    group_by(Replicate)%>%
+    summarize("AUC" = sum(Area), "Peak" = max(Peaks),
+              "Time_Peak_Start" = min(Time), "Time_Peak_End" = max(Time)) %>%
+    arrange(Time_Peak_Start)
+
+  # Create a summary that includes the sample name + replicate number, date
+  # of injection, and total AUC.
+  auc_summary <- data.frame(sample =
+                              str_sub(basename(file_path), start = 10, end =-13),
+                            date = str_sub(data.1$Time[1],
+                                           end = 10),
+                            total_auc = sum(test.4$AUC))
+  return(auc_summary)
+}
+
