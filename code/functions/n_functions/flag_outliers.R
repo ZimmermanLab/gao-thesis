a16e1fc0147bb53415ca8ec816705fbfd8f1ed00
Sarah Gao <sgao22@usfca.edu>
2022-10-31 16:51:04 -0700

    Remove median + IQR mutate

--- code/functions/n_functions/flag_outliers.R
+++ code/functions/n_functions/flag_outliers.R
@@ -11,13 +11,7 @@ library("dplyr")
 library("tidyr")
 
 flag_outliers <- function(clean_dataset) {
-  clean_stats <- clean_dataset %>%
-    group_by(sample_no) %>%
-    mutate(median_nh3 = median(nh3),
-           median_no2_no3 = median(no2_no3),
-           iqr_nh3 = IQR(nh3),
-           iqr_no2_no3 = IQR(no2_no3))
-  outlier_flags <- clean_stats %>%
+  outlier_flags <- clean_dataset %>%
     mutate(outlier_flag = case_when(nh3 > (median_nh3 + 3 * iqr_nh3) |
                                       nh3 < (median_nh3 - 3 * iqr_nh3) |
                                       no2_no3 > (
