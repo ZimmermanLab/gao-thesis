df46ba0894ca0f6effa49abd17dc0220d9abeac8
Sarah Gao <sgao22@usfca.edu>
2022-11-19 15:50:38 -0800

    Take out ggsave from function

--- code/functions/n_functions/summarize_treat_n_data.R
+++ code/functions/n_functions/summarize_treat_n_data.R
@@ -34,28 +34,24 @@ sum_plot_n <- function(n_data, y_var, n_type) {
   n_plot <- n_data %>%
     filter(!is.na(!! sym(y_var))) %>%
     ggplot(aes(x = factor(cc_treatment, levels = c("no_cc", "w_cc")),
-               y = (!! sym(y_var)),
-               fill = cc_treatment,
-               color = cc_treatment)) +
-    geom_boxplot() +
+               y = (!! sym(y_var)))) +
+    geom_boxplot(aes(fill = cc_treatment,
+                     color = cc_treatment)) +
     facet_grid(~ factor(drying_treatment,
                         levels = c("initial", "one_wk", "two_wk", "four_wk")),
                labeller = facet_drying_labels)  +
     scale_fill_manual(name = NULL, limits = c("no_cc", "w_cc"),
                       values = c("#16B4FF", "#34980D"),
-                      labels = c("No Cover Crop", "With Cover Crop")) +
+                      labels = c("No Cover Crop   ", "With Cover Crop")) +
     scale_color_manual(name = NULL, limits = c("no_cc", "w_cc"),
                        values = c("#097CB2", "#195004"),
-                       labels = c("No Cover Crop", "With Cover Crop")) +
+                       labels = c("No Cover Crop   ", "With Cover Crop")) +
     theme(axis.text.x = element_blank(),
           axis.title.x = element_blank(),
           axis.ticks.x = element_blank(),
           legend.position = "bottom") +
     labs(y = y_title,
          title = title)
-
-  ggsave(n_plot, filename = paste0("output/2022/n_plots/", y_var, ".png"),
-         width = 14, height = 8, units = "in")
 
 # Create median and IQR summary of data
 treat_sum <- n_data %>%
