7202757b25fba91abf3c5427b6716775ea4fff36
Sarah Gao <sgao22@usfca.edu>
2022-11-19 13:21:34 -0800

    Combine plots with patchwork and use same y-axis

--- code/co2_pairwise_rewet.R
+++ code/co2_pairwise_rewet.R
@@ -5,6 +5,11 @@
 # November 18th, 2022
 # hellosarahgao@gmail.com
 
+library("ggplot2")
+library("dplyr")
+library("patchwork")
+library("gridExtra")
+
 # Source function and set plot themes
 source("code/functions/set_plot_themes.R")
 set_theme("pres")
@@ -29,10 +34,14 @@ nocc_one <- co2_nocc_rewet %>%
                    p_val_cutoff = 0.1,
                    fill_value = "#16B4FF",
                    col_value = "#097CB2") +
-  labs(x = NULL, y = NULL) +
+  labs(x = NULL, y = NULL,
+       title = "One Week") +
   scale_fill_manual(values = "#16B4FF") +
   scale_color_manual(values = "#097CB2") +
-  scale_y_continuous(limits = c(0, 5840))
+  scale_y_continuous(limits = c(0, 9200),
+                     labels = label_comma()) +
+  theme(plot.margin = unit(c(0, 0, 0, 0), "cm"),
+        plot.title = element_text(size = 20))
 
 # Filter for two weeks for second plot
 nocc_two <- co2_nocc_rewet %>%
@@ -42,10 +51,15 @@ nocc_two <- co2_nocc_rewet %>%
                    p_val_cutoff = 0.1,
                    fill_value = "#16B4FF",
                    col_value = "#097CB2") +
-  labs(x = NULL, y = NULL) +
+  labs(x = NULL, y = NULL,
+       title = "Two Weeks") +
   scale_fill_manual(values = "#16B4FF") +
   scale_color_manual(values = "#097CB2") +
-  scale_y_continuous(limits = c(0, 9200))
+  scale_y_continuous(limits = c(0, 9200)) +
+  theme(plot.margin = unit(c(0, 0, 0, 0.3), "cm"),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 16))
 
 # Filter for four weeks for third plot
 nocc_four <- co2_nocc_rewet %>%
@@ -55,17 +69,25 @@ nocc_four <- co2_nocc_rewet %>%
                    p_val_cutoff = 0.1,
                    fill_value = "#16B4FF",
                    col_value = "#097CB2") +
-  labs(x = NULL, y = NULL) +
+  labs(x = NULL, y = NULL,
+       title = "Four Weeks") +
   scale_fill_manual(values = "#16B4FF") +
   scale_color_manual(values = "#097CB2") +
-  scale_y_continuous(limits = c(0, 5530))
+  scale_y_continuous(limits = c(0, 9200)) +
+  theme(plot.margin = unit(c(0, 0, 0, 0.3), "cm"),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 16)) +
+  # Add test annotation
+  annotate("text", x = 4.5, y = 9000, label = "Dunn's Test",
+           size = 5, family = "Helvetica")
 
-# put the subplots together
-combined_plot <- plot_grid(nocc_one, nocc_two, nocc_four,
-                           labels = c("One Week", "Two Weeks", "Four Weeks"),
-                           label_size = 12, nrow = 1, align = "hv", label_x = 0.38) +
-  # have to adjust this on top to make space for subplot labels
-  theme(plot.margin = unit(c(0.5, 0, 0, 0), "cm")) # top, right, bottom, left
+# Combine plots together
+patchwork_plot <- (nocc_one + nocc_two + nocc_four) +
+  plot_annotation(
+    title = "CO2 After Rewetting Without Cover Crop",
+    theme = theme(plot.title = element_text(size = 24),
+                  plot.margin = margin(unit(c(0, 0, 15, 15), "cm"))))
 
 # combined label for x axis
 x.grob <- textGrob("Days After Rewetting",
@@ -80,6 +102,11 @@ y.grob <- textGrob("CO2 (ppm)",
                              fontfamily = "Georgia"),
                    rot = 90)
 
-# add all the things to plot
-grid.arrange(arrangeGrob(combined_plot, left = y.grob, bottom = x.grob))
-
+# Add axes labels to plot
+pw_plot <- patchworkGrob(patchwork_plot)
+nocc_rewet_plot <- grid.arrange(arrangeGrob(pw_plot,
+                                left = y.grob,
+                                bottom = x.grob),
+                                vp = viewport(width = 0.95, height = 0.95))
+ggsave(nocc_rewet_plot, filename = "output/2022/co2/figures/co2_rewet_nocc.png",
+       width = 14, height = 8, units = "in")
