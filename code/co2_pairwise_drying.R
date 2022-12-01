aaf539889037444c7cafcdb0e84c17e4b2c0ca3e
Sarah Gao <sgao22@usfca.edu>
2022-12-01 14:38:36 -0800

    Change plots to thesis formatting, attempt to do stats+facets

--- code/co2_pairwise_drying.R
+++ code/co2_pairwise_drying.R
@@ -15,23 +15,26 @@ library("gridExtra") # for multiple plots
 source("code/functions/custom_few_theme.R")
 source("code/functions/pairwise_compare.R")
 source("code/functions/set_plot_themes.R")
-set_theme("pres")
+set_theme("doc")
 
 #### CO2 IN DRYING SOILS ####
 
 # Read in CO2 data
-co2_data <- read_csv("data/cleaned_data/LICOR/samp_medians.csv")
-
-# Subset to no cc, drying only
-nocc_co2 <- co2_data %>%
-  filter(drying_treatment == "initial_dry") %>%
-  filter(cc_treatment == "no_cc") %>%
+co2_data <- read_csv("data/cleaned_data/LICOR/samp_medians.csv") %>%
   # Populate day_elapsed column
   mutate(day_elapsed = case_when(date == "2022-02-15" ~ "1",
                                  date == "2022-02-16" ~ "2",
                                  date == "2022-02-17" ~ "3",
                                  date == "2022-02-18" ~ "4",
                                  date == "2022-02-19" ~ "5"))
+
+# Subset to no cc, drying only
+nocc_co2 <- co2_data %>%
+  filter(drying_treatment == "initial_dry") %>%
+  filter(cc_treatment == "no_cc")
+wcc_co2 <- co2_data %>%
+  filter(drying_treatment == "initial_dry") %>%
+  filter(cc_treatment == "w_cc")
 
 # Run Dunn's test and generate plot
 nocc_co2_plot <- nocc_co2 %>%
@@ -43,17 +46,33 @@ nocc_co2_plot <- nocc_co2 %>%
   # Use blue fill and stroke for no_cc palette
   scale_fill_manual(values = "#16B4FF") +
   scale_color_manual(values = "#097CB2") +
-  scale_y_continuous(labels = label_comma(),
-                     limits = c(2000, 5300)) +
   labs(x = "Day of Drying",
        y = "CO2 (ppm)",
-       title = "CO2 in Drying Soils") +
+       title = "CO2 in Drying Soils")
   # Add test annotation
-  annotation_custom(grob =
-                      grobTree(textGrob("Dunn's Test",
-                                        x = .8, y = 0.85,
-                                        gp = gpar(fontsize = 16,
-                                                  fontfamily = "Helvetica",
-                                                  lineheight = 0.9))))
+  # annotation_custom(grob =
+  #                    grobTree(textGrob("Dunn's Test",
+  #                                      x = .8, y = 0.85,
+  #                                      gp = gpar(fontsize = 16,
+  #                                                fontfamily = "Helvetica",
+  #                                                lineheight = 0.9))))
+
+wcc_co2_plot <- wcc_co2 %>%
+  ggplot(aes(x = day_elapsed,
+             y = median,
+             fill = "#34980D",
+             color = "#195004")) +
+  geom_boxplot() +
+  # Use blue fill and stroke for no_cc palette
+  scale_fill_manual(values = "#34980D") +
+  scale_color_manual(values = "#195004") +
+  scale_y_continuous(labels = label_comma(),limits = c(1, 1000000),
+                     trans = pseudo_log_trans(base = 10)) +
+  theme(legend.position = "none") +
+  labs(x = "Day of Drying",
+       y = "CO2 (ppm)",
+       title = "CO2 in Drying Soils")
+
+
 ggsave(nocc_co2_plot, filename = "output/2022/co2/figures/co2_drying_nocc.png",
        width = 8, height = 8, units = "in")
