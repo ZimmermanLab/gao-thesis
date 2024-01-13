98f4daeafaaaccf6ff290e235150a2da9f7068a0
Sarah Gao <sgao22@usfca.edu>
2024-01-12 17:28:09 -0800

    Add columns + pivot longer to set up master dataframe

--- code/functions/ea_functions/clean_ea_data.R
+++ code/functions/ea_functions/clean_ea_data.R
@@ -23,29 +23,55 @@ clean_ea_data <- function(input_file_list) {
   if (stri_detect_fixed(input_file_list[1], "percent") == TRUE) {
     ea_results_clean <- ea_results_raw %>%
       select("...2", "...4", "...6", "...12", "...13") %>%
-      rename("sample_no" = "...2", "date" = "...4", "type" = "...6",
-             "n_per" = "...12", "c_per" = "...13") %>%
-      drop_na() %>%
+      rename("sample_id" = "...2", "analyzed_date" = "...4", "type" = "...6",
+             "nitrogen_pcnt" = "...12", "carbon_pcnt" = "...13") %>%
       # Filter out any samples and/or SRM runs that were 0
-      filter(!(str_detect(sample_no, "SG") & n_per == 0)) %>%
-      filter(!(str_detect(sample_no, "SRM") & n_per == 0))
-
-  } else if (stri_detect_fixed(input_file_list[1], "ratio") == TRUE) {
+      filter(!(str_detect(sample_id, "SG") & nitrogen_pcnt == 0)) %>%
+      filter(!(str_detect(sample_id, "SRM") & nitrogen_pcnt == 0)) %>%
+      # Pivot to long format
+      pivot_longer(cols = c(nitrogen_pcnt, carbon_pcnt),
+                   names_to = "subtype", values_to = "value")
+  }
+  # FOR RATIO DATA:
+  else if (stri_detect_fixed(input_file_list[1], "ratio") == TRUE) {
       ea_results_clean <- ea_results_raw %>%
         select("...2", "...4", "...6", "...13") %>%
-        rename("sample_no" = "...2", "date" = "...4", "type" = "...6",
+        rename("sample_id" = "...2", "analyzed_date" = "...4", "type" = "...6",
                "c_n_ratio" = "...13") %>%
-        drop_na() %>%
       # Filter out any samples and/or SRM runs that were 0
-      filter(!(str_detect(sample_no, "SG") & c_n_ratio == 0)) %>%
-      filter(!(str_detect(sample_no, "SRM") & c_n_ratio == 0))
+      filter(!(str_detect(sample_id, "SG") & c_n_ratio == 0)) %>%
+      filter(!(str_detect(sample_id, "SRM") & c_n_ratio == 0)) %>%
+      # Pivot to long format
+      pivot_longer(cols = c_n_ratio,
+                   names_to = "subtype", values_to = "value")
   }
+
   # Clean up sample names
   ea_results_clean <- ea_results_clean %>%
-    mutate(sample_no = case_when(
-    str_detect(sample_no, "SG") ~
-      str_sub(sample_no, start = -3),
-    TRUE ~ sample_no))
+    mutate(sample_id = case_when(
+    str_detect(sample_id, "SG") ~
+      str_sub(sample_id, start = -3),
+    TRUE ~ sample_id)) %>%
+    # Reformat date column
+    mutate(analyzed_date = strftime(as.Date(
+      analyzed_date, "%m/%d/%Y"),"%Y%m%d")) %>%
+    # Filter out NA values
+    drop_na() %>%
+    # Add sample type column
+    mutate(sample_type = "core") %>%
+    # Add measurement type column
+    mutate(measurement_type = "ea") %>%
+    # Add standard / sample / blank column
+    mutate(standard_sample_blank = case_when(
+      type == "STD" ~ "standard",
+      (type == "UNK" & str_detect(sample_id, "B")) ~ "blank",
+      (type == "UNK" & str_detect(sample_id, "SRM")) ~ "standard",
+      .default = "sample")) %>%
+    # Add column to include units
+    mutate(units = NA) %>%
+    # Rearrange columns and drop "type" column
+    select(sample_id, sample_type, analyzed_date, measurement_type, subtype,
+           standard_sample_blank, value, units)
 
   return(ea_results_clean)
 }
