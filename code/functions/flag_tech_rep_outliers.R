5b2755fe07004b917bb6c452475fc35328060b9e
Sarah Gao <sgao22@usfca.edu>
2024-04-08 22:44:32 -0700

    Add outlier column to samples only dataset

--- code/functions/flag_tech_rep_outliers.R
+++ code/functions/flag_tech_rep_outliers.R
@@ -11,18 +11,17 @@ library("tidyr")
 library("dplyr")
 
 flag_outliers <- function(samples_only) {
-  outlier_flags <- clean_data %>%
-    group_by(sample_no) %>%
-    mutate(median_n = median(n_per),
-           median_c = median(c_per),
-           iqr_n = IQR(n_per),
-           iqr_c = IQR(c_per)) %>%
-    mutate(outlier_flags_per = case_when(n_per > (median_n + 3 * iqr_n) |
-                                           n_per < (median_n - 3 * iqr_n) |
-                                           c_per > (median_c + 3 * iqr_c) |
-                                           c_per < (median_c - 3 * iqr_c) ~ "extreme",
-                                         n_per > (median_n + 1.5 * iqr_n) |
-                                           n_per < (median_n - 1.5 * iqr_n) |
-                                           c_per > (median_c + 1.5 * iqr_c) |
-                                           c_per < (median_c - 1.5 * iqr_c) ~ "moderate"))
+  outlier_flags <- samples_only %>%
+    group_by(sample_id, measurement_type, subtype, subsubtype,
+             run_or_plate_id, analyzed_date) %>%
+    mutate(median = median(value, na.rm = TRUE),
+           iqr = IQR(value, na.rm = TRUE)) %>%
+    mutate(outlier_flags = case_when(value > (median + 3 * iqr) |
+                                           value < (median - 3 * iqr)
+                                         ~ "extreme",
+                                         value > (median + 1.5 * iqr) |
+                                           value < (median - 1.5 * iqr)
+                                         ~ "moderate"))
+
+  return(outlier_flags)
 }
