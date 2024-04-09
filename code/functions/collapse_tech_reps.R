1c6a1ddce6a630a5f6a545ec619e5bf1825b7eb3
Sarah Gao <sgao22@usfca.edu>
2024-04-08 23:03:19 -0700

    Collapse tech reps per sample test

---
+++ code/functions/collapse_tech_reps.R
@@ -0,0 +1,32 @@
+# This function takes in sample-only data with outlier flags and collapses it
+# by technical replicates.
+# The outlier_flag parameter specifies which outlier you want to filter out
+# with the default being no filtering.
+
+# Sarah Gao
+# April 8th, 2024
+# hellosarahgao@gmail.com
+
+collapse_tech_reps <- function(dataset, outlier_flag = "none"){
+
+  # Filters out tech reps that are extreme outliers
+  if(outlier_flag == "extreme"){
+    dataset <- dataset %>%
+      filter(outlier_flags == "moderate" |
+               is.na(outlier_flags))
+  }
+
+  # Filters out tech reps that are extreme and moderate outliers
+  if(outlier_flag == "moderate"){
+    dataset <- dataset %>%
+      filter(is.na(outlier_flags))
+  }
+
+  # Find mean value for each sample for each test across all tech reps
+  collapsed_samples <- dataset %>%
+    group_by(sample_id, measurement_type, subtype, subsubtype,
+             run_or_plate_id, analyzed_date) %>%
+    summarise(mean_value = mean(value, na.rm = TRUE))
+
+  return(collapsed_samples)
+}
